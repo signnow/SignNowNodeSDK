efc6da2a3e01965a808b508ac0a656fcb02eb2e5
Ruslan Tereshchenko <tereshchenko.ruslan@pdffiller.team>
2019-06-25 16:23:45 +0300

    [ter] fixed test cases after linting

--- test/document.test.js
+++ test/document.test.js
@@ -1,183 +1,178 @@
+'use strict';
+const settings = require('../test-settings').settings;
+const signnow = require('../lib/signnow')({
+  credentials: settings.credentials,
+  production: settings.production, // (false by defult)
+});
+
 (function() {
-    "use strict";
 
-    var settings = require('../test-settings').settings;
-    var signnow = require('../lib/signnow')({
-            credentials: settings.credentials,
-            production: settings.production //(false by defult)
-        }),
-        should = require('chai').should();
+  describe('document', () => {
 
-
-    describe('document', function() {
-
-        describe('.list()', function() {
-            it('should return a list of documents', function(done) {
-                this.timeout(10000);
-                signnow.document.list({
-                    "token": settings.token
-                }, function(err, res) {
-                    if (err) throw err[0].message;
-                    res.should.be.a('array');
-                    res[0].should.have.property("document_name");
-                    done();
-                });
-            });
+    describe('.list()', () => {
+      it('should return a list of documents', done => {
+        signnow.document.list({
+          'token': settings.token,
+        }, (err, res) => {
+          if (err) {throw err[0].message;}
+          res.should.be.a('array');
+          res[0].should.have.property('document_name');
+          done();
         });
-
-        describe('.view()', function() {
-            it('should return a document resource', function(done) {
-                signnow.document.view({
-                    "token": settings.token,
-                    "id": settings.documentid
-                }, function(err, res) {
-                    if (err) throw err[0].message;
-                    res.should.have.property("document_name");
-                    done();
-                });
-            });
-        });
-
-        describe('.download()', function() {
-            it('should download a collapsed document', function(done) {
-                signnow.document.download({
-                    "token": settings.token,
-                    "id": settings.documentid
-                }, function(err, res) {
-                    if (err) throw err[0].message;
-                    res.should.exist();
-                    done();
-                });
-            });
-        });
-
-        describe('.update()', function() {
-            it('should update a document and return the document id', function(done) {
-                var fields = {
-                    "texts": [{
-                        "size": 8,
-                        "x": 61,
-                        "y": 72,
-                        "page_number": 0,
-                        "font": "Arial",
-                        "data": "sample text",
-                        "line_height": 9.075
-                    }]
-                };
-
-                signnow.document.update({
-                    "token": settings.token,
-                    "id": settings.documentid,
-                    "fields": fields
-                }, function(err, res) {
-                    if (err) throw err[0].message;
-                    res.should.have.property("id");
-                    done();
-                });
-            });
-        });
-
-        describe.skip('.invite()', function() {
-            it('should send an invite to sign a document', function(done) {
-                signnow.document.invite({
-                    "token": settings.token,
-                    "id": settings.documentid,
-                    "from": settings.email,
-                    "to": settings.testemail
-                }, function(err, res) {
-                    if (err) throw err[0].message;
-                    res.should.have.property("result", "success");
-                    done();
-                });
-            });
-        });
-
-        describe('.cancelInvite()', function() {
-            it('should cancel all invites to a document', function(done) {
-                signnow.document.cancelInvite({
-                    "token": settings.token,
-                    "id": settings.documentid
-                }, function(err, res) {
-                    if (err) throw err[0].message;
-                    res.should.have.property("status", "success");
-                    done();
-                });
-            });
-        });
-
-        describe('.share()', function() {
-            it('should return a link to download the document', function(done) {
-                signnow.document.share({
-                    "token": settings.token,
-                    "id": settings.documentid
-                }, function(err, res) {
-                    if (err) throw err[0].message;
-                    res.should.have.property("link");
-                    done();
-                });
-            });
-        });
-
-        describe.skip('.merge()', function() {
-            it('should merge two documents and return the new document name', function(done) {
-                signnow.document.merge({
-                    "token": settings.token,
-                    "name": "Mocha Unit Test",
-                    "document_ids": [
-                        "xxx",
-                        "xxx"
-                    ]
-                }, function(err, res) {
-                    if (err) throw err[0].message;
-                    res.should.have.property("name");
-                    res.should.have.property("document_ids");
-                    done();
-                });
-            });
-        });
-
-        describe.skip('.create()', function() {
-            this.timeout(20000);
-            it('should upload a file and create a new document', function(done) {
-                signnow.document.create({
-                    "token": settings.token,
-                    "filepath": __dirname + "/test.doc"
-                }, function(err, res) {
-                    if (err) return done(err[0].message);
-                    res.should.have.property("id");
-                    done();
-                });
-            });
-        });
-
-        describe.skip('.extractfields()', function() {
-            this.timeout(20000);
-            it('should upload a file containing SignNow field tags', function(done) {
-                signnow.document.extractfields({
-                    "token": settings.token,
-                    "filepath": __dirname + "/test.doc"
-                }, function(err, res) {
-                    if (err) return done(err[0].message);
-                    res.should.have.property("id");
-                    done();
-                });
-            });
-        });
-
-        describe.skip('.history()', function() {
-            it('should return the history of a document', function(done) {
-                signnow.document.history({
-                    "token": settings.token,
-                    "id": settings.documentid
-                }, function(err, res) {
-                    if (err) return done(err[0].message);
-                    res.should.be.a('array');
-                    res[0].should.have.property("event");
-                    done();
-                });
-            });
-        });
-
+      }).timeout(10000);
     });
+
+    describe('.view()', () => {
+      it('should return a document resource', done => {
+        signnow.document.view({
+          'token': settings.token,
+          'id': settings.documentid,
+        }, (err, res) => {
+          if (err) {throw err[0].message;}
+          res.should.have.property('document_name');
+          done();
+        });
+      });
+    });
+
+    describe('.download()', () => {
+      it('should download a collapsed document', done => {
+        signnow.document.download({
+          'token': settings.token,
+          'id': settings.documentid,
+        }, (err, res) => {
+          if (err) {throw err[0].message;}
+          res.should.exist();
+          done();
+        });
+      });
+    });
+
+    describe('.update()', () => {
+      it('should update a document and return the document id', done => {
+        const fields = {
+          'texts': [{
+            'size': 8,
+            'x': 61,
+            'y': 72,
+            'page_number': 0,
+            'font': 'Arial',
+            'data': 'sample text',
+            'line_height': 9.075,
+          }],
+        };
+
+        signnow.document.update({
+          'token': settings.token,
+          'id': settings.documentid,
+          'fields': fields,
+        }, (err, res) => {
+          if (err) {throw err[0].message;}
+          res.should.have.property('id');
+          done();
+        });
+      });
+    });
+
+    describe.skip('.invite()', () => {
+      it('should send an invite to sign a document', done => {
+        signnow.document.invite({
+          'token': settings.token,
+          'id': settings.documentid,
+          'from': settings.email,
+          'to': settings.testemail,
+        }, (err, res) => {
+          if (err) {throw err[0].message;}
+          res.should.have.property('result', 'success');
+          done();
+        });
+      });
+    });
+
+    describe('.cancelInvite()', () => {
+      it('should cancel all invites to a document', done => {
+        signnow.document.cancelInvite({
+          'token': settings.token,
+          'id': settings.documentid,
+        }, (err, res) => {
+          if (err) {throw err[0].message;}
+          res.should.have.property('status', 'success');
+          done();
+        });
+      });
+    });
+
+    describe('.share()', () => {
+      it('should return a link to download the document', done => {
+        signnow.document.share({
+          'token': settings.token,
+          'id': settings.documentid,
+        }, (err, res) => {
+          if (err) {throw err[0].message;}
+          res.should.have.property('link');
+          done();
+        });
+      });
+    });
+
+    describe.skip('.merge()', () => {
+      it('should merge two documents and return the new document name', done => {
+        signnow.document.merge({
+          'token': settings.token,
+          'name': 'Mocha Unit Test',
+          'document_ids': [
+            'xxx',
+            'xxx',
+          ],
+        }, (err, res) => {
+          if (err) {throw err[0].message;}
+          res.should.have.property('name');
+          res.should.have.property('document_ids');
+          done();
+        });
+      });
+    });
+
+    describe.skip('.create()', () => {
+      it('should upload a file and create a new document', done => {
+        signnow.document.create({
+          'token': settings.token,
+          'filepath': __dirname + '/test.doc',
+        }, (err, res) => {
+          if (err) {return done(err[0].message);}
+          res.should.have.property('id');
+          done();
+        });
+      }).timeout(20000);
+    });
+
+    describe.skip('.extractfields()', () => {
+      it('should upload a file containing SignNow field tags', done => {
+        signnow.document.extractfields({
+          'token': settings.token,
+          'filepath': __dirname + '/test.doc',
+        }, (err, res) => {
+          if (err) {return done(err[0].message);}
+          res.should.have.property('id');
+          done();
+        });
+      }).timeout(20000);
+    });
+
+    describe.skip('.history()', () => {
+      it('should return the history of a document', done => {
+        signnow.document.history({
+          'token': settings.token,
+          'id': settings.documentid,
+        }, (err, res) => {
+          if (err) {return done(err[0].message);}
+          res.should.be.a('array');
+          res[0].should.have.property('event');
+          done();
+        });
+      });
+    });
+
+  });
 
 })();
--- test/webhook.test.js
+++ test/webhook.test.js
@@ -1,45 +1,43 @@
+'use strict';
+const settings = require('../test-settings').settings;
+const signnow = require('../lib/signnow')({
+  credentials: settings.credentials,
+  production: settings.production, // (false by defult)
+});
+
 (function() {
-    "use strict";
 
-    var settings = require('../test-settings').settings;
-    var signnow = require('../lib/signnow')({
-            credentials: settings.credentials,
-            production: settings.production //(false by defult)
-        }),
-        should = require('chai').should();
+  describe('webhook', () => {
 
-
-    describe('webhook', function() {
-
-        describe('.list()', function() {
-            it('should return a list of webhooks', function(done) {
-                signnow.webhook.list({
-                    "token": settings.token
-                }, function(err, res) {
-                    if (err) throw err[0].message;
-                    res.should.be.a('object');
-                    res.should.have.property("subscriptions");
-                    done();
-                });
-            });
+    describe('.list()', () => {
+      it('should return a list of webhooks', done => {
+        signnow.webhook.list({
+          'token': settings.token,
+        }, (err, res) => {
+          if (err) {throw err[0].message;}
+          res.should.be.a('object');
+          res.should.have.property('subscriptions');
+          done();
         });
-
-        describe('.create()', function() {
-            it('should create a new webhook and return an id', function(done) {
-                signnow.webhook.create({
-                    "token": settings.token,
-                    "event": "document.create",
-                    "callback_url": "http://requestb.in/qwmphiqw"
-                }, function(err, res) {
-                    if (err) throw err[0].message;
-                    res.should.be.a("object");
-                    res.should.have.property("id");
-                    done();
-                });
-            });
-        });
-
-
+      });
     });
+
+    describe('.create()', () => {
+      it('should create a new webhook and return an id', done => {
+        signnow.webhook.create({
+          'token': settings.token,
+          'event': 'document.create',
+          'callback_url': 'http://requestb.in/qwmphiqw',
+        }, (err, res) => {
+          if (err) {throw err[0].message;}
+          res.should.be.a('object');
+          res.should.have.property('id');
+          done();
+        });
+      });
+    });
+
+
+  });
 
 })();
