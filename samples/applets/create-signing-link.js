45d1f6442752e3976aee3d53596f12e28c248d50
ZolotovPDFfiller <zolotov.artem@prffiller.team>
2019-07-22 10:37:11 +0300

    - Created signing link url applet - Created signing link docs - Create template methods name changed (for docs) - Updated CHANGELOG - esdoc link file added

---
+++ samples/applets/create-signing-link.js
@@ -0,0 +1,85 @@
+/*
+ * to run create signing link applet from the project root folder type in your console:
+ * > node samples/applets/create-signing-link <cliend_id> <client_secret> <username> <password> <path_to_file> <fields_stringified>
+ * <cliend_id>, <client_secret>, <username>, <password>, <path_to_file>, <fields_stringified> - are required params
+ */
+
+'use strict';
+
+const [
+  clientId,
+  clientSecret,
+  username,
+  password,
+  filepath,
+  fieldsStringified
+] = process.argv.slice(2);
+
+const api = require('../../lib')({
+  credentials: Buffer.from(`${clientId}:${clientSecret}`).toString('base64'),
+  production: false,
+});
+
+const { oauth2: { requestToken: getAccessToken } } = api;
+const { link: { create: documentLink } } = api;
+const {
+  document: {
+    create: documentUpload,
+    update: addFields,
+  }
+} = api;
+
+getAccessToken({
+  username,
+  password,
+}, (tokenErr, tokenRes) => {
+  if (tokenErr) {
+    console.error(tokenErr);
+  } else {
+    const { access_token: token } = tokenRes;
+
+    documentUpload({
+      filepath,
+      token,
+    }, (uploadErr, uploadRes) => {
+      if (uploadErr) {
+        console.error(uploadErr);
+      } else {
+        const { id } = uploadRes;
+        const client_timestamp = Math.floor(Date.now() / 1000);
+        let fields;
+
+        try {
+          fields = JSON.parse(fieldsStringified);
+        } catch (err) {
+          console.log(err);
+          return
+        }
+
+        addFields({
+          id,
+          fields: {
+            client_timestamp,
+            fields
+          },
+          token
+        }, updateErr => {
+          if (updateErr) {
+            console.log(updateErr);
+          } else {
+            documentLink({
+              document_id: id,
+              token
+            }, (linkErr, linkRes) => {
+              if (linkErr) {
+                console.log(linkErr)
+              } else {
+                console.log(linkRes)
+              }
+            })
+          }
+        })
+      }
+    });
+  }
+});
