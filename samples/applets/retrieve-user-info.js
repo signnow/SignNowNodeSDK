30137d383e303e6c8b67f01a72ed87193517b927
ZolotovPDFfiller <zolotov.artem@prffiller.team>
2019-07-22 17:41:48 +0300

    Retrieve user information applet  - Updated CHANGELOG.md

---
+++ samples/applets/retrieve-user-info.js
@@ -0,0 +1,50 @@
+/*
+ * to run retrieve-user-info applet from the project root folder type in your console:
+ * > node samples/applets/retrieve-user-info <cliend_id> <client_secret> <email> <password> <first_name> <last_name>
+ * <email> <password> - are required params
+ * <first_name> <last_name> - are optional
+ */
+
+'use strict';
+
+const [
+  clientId,
+  clientSecret,
+  username,
+  password
+] = process.argv.slice(2);
+
+const api = require('../../lib')({
+  credentials: Buffer.from(`${clientId}:${clientSecret}`).toString('base64'),
+  production: false,
+});
+
+const {
+  user: {
+    retrieve: getUserInfo
+  },
+  oauth2: {
+    requestToken: getAccessToken
+  }
+} = api;
+
+getAccessToken({
+  username,
+  password
+}, (tokenErr, tokenRes) => {
+  if (tokenErr) {
+    console.error(tokenErr);
+  } else {
+    const { access_token: token } = tokenRes;
+
+    getUserInfo({
+      token
+    }, (getInfoErr, getInfoRes) => {
+      if (getInfoErr) {
+        console.error(getInfoErr);
+      } else {
+        console.log(getInfoRes)
+      }
+    })
+  }
+});
