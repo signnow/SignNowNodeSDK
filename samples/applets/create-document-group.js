7a55f45caa5f9b0a14f8a596daee6bdcf1e924a7
ZolotovPDFfiller <zolotov.artem@prffiller.team>
2019-08-05 15:53:42 +0300

    Remade 'Create Document Group' applet - updated README - 'Create Document Group' docs updated

--- samples/applets/create-document-group.js
+++ samples/applets/create-document-group.js
@@ -1,8 +1,7 @@
 /*
  * to run create document group applet from the project root folder type in your console:
- * > node samples/applets/create-document-group <cliend_id> <client_secret> <username> <password> <group_name> <signature_stringified> <filepath>
- * <cliend_id>, <client_secret>, <username>, <password>, <group_name>, <signature_stringified> - are required params
- * <filepath> - is optional param. If empty, default will be './samples/files/pdf-sample.pdf'
+ * > node samples/applets/create-document-group <cliend_id> <client_secret> <token> <group_name> <document_ids>
+ * <cliend_id>, <client_secret>, <token>, <group_name>, <document_ids> - are required params
  */
 
 'use strict';
@@ -10,11 +9,9 @@
 const [
   clientId,
   clientSecret,
-  username,
-  password,
+  token,
   group_name,
-  fieldsStringifed,
-  filepath = './samples/files/pdf-sample.pdf',
+  ...document_ids
 ] = process.argv.slice(2);
 
 const api = require('../../lib')({
@@ -22,81 +19,16 @@ const api = require('../../lib')({
   production: false,
 });
 
-const {
-  documentGroup: { create: createDocumentGroup },
-  oauth2: { requestToken: getAccessToken },
-  document: {
-    create: createDocument,
-    update: addFields,
-  }
-} = api;
+const { documentGroup: { create: createDocumentGroup } } = api;
 
-getAccessToken({
-  username,
-  password
-}, (tokenErr, tokenRes) => {
-  if (tokenErr) {
-    console.error(tokenErr);
+createDocumentGroup({
+  token,
+  document_ids,
+  group_name,
+}, (createGroupErr, createGroupRes) => {
+  if (createGroupErr) {
+    console.error(createGroupErr);
   } else {
-    const { access_token: token } = tokenRes;
-
-    createDocument({
-      filepath,
-      token
-    }, (createError, createResponse) => {
-      if (createError) {
-        console.error(createError);
-      } else {
-        const { id } = createResponse;
-        const client_timestamp = Math.floor(Date.now() / 1000);
-
-        let fields;
-
-        try {
-          fields = JSON.parse(fieldsStringifed)
-        } catch (err) {
-          console.error(err);
-          return;
-        }
-
-        addFields({
-          fields: {
-            client_timestamp,
-            fields,
-          },
-          id,
-          token,
-        }, (addFieldsErr, addFieldsRes) => {
-          if (addFieldsErr) {
-            console.error(addFieldsErr);
-          } else {
-             const { id: signedId } = addFieldsRes;
-
-            createDocument({
-              filepath,
-              token
-            }, (createError, createResponse) => {
-              if (createError) {
-                console.error(createError);
-              } else {
-                const { id } = createResponse;
-
-                createDocumentGroup({
-                  token,
-                  document_ids: [signedId, id],
-                  group_name
-                }, (createDocGroupErr, createDocGroupRes) => {
-                  if (createDocGroupErr) {
-                    console.error(createDocGroupErr);
-                  } else {
-                    console.log(createDocGroupRes);
-                  }
-                })
-              }
-            })
-          }
-        })
-      }
-    })
+    console.log(createGroupRes);
   }
 });
