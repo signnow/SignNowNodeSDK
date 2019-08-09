15bdc8d54d840f2fe5ade72eb73d2e94eb4637a4
Ruslan Tereshchenko <tereshchenko.ruslan@pdffiller.team>
2019-08-09 12:28:58 +0300

    describe Document History feature and create CLI applet

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Added
 
+- Updated documentation with *Get Document History* feature description
+
 ## [v1.4.0] - 2019-08-08
 
 ### Added
--- samples/snippets/getDocumentHistory.js
+++ samples/snippets/getDocumentHistory.js
@@ -12,20 +12,20 @@ const token = 'YOUR_ACCESS_TOKEN';
 /**
  * @typedef {Object} DocumentEvent
  * @property {string} client_app_name
- * @property {string} client_timestamp
+ * @property {?string} client_timestamp
  * @property {string} created - timestamp of action creation
  * @property {string} email - email of actor
  * @property {string} event
  * @property {string} ip_address
- * @property {?string} document_id
  * @property {?string} element_id
  * @property {?string} field_id
  * @property {?string} json_attributes
- * @property {?string} origin
- * @property {?string} unique_id
- * @property {?string} user_agent
- * @property {?string} user_id - id of actor
- * @property {?string} version
+ * @property {string} [document_id]
+ * @property {string} [origin]
+ * @property {string} [unique_id]
+ * @property {string} [user_agent]
+ * @property {string} [user_id] - id of actor
+ * @property {string} [version] - version of signed document (incremented after each signature addition)
  */
 
 /**
