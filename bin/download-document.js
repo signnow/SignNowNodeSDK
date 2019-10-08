ee736e056381a6dce530be1a0bbc233d54376870
Артем <42999342+Temu4@users.noreply.github.com>
2019-10-08 15:49:47 +0300

    Update bin/download-document.js
    Co-Authored-By: Ruslan Tereshchenko <tereshchenko.ruslan@pdffiller.team>

--- bin/download-document.js
+++ bin/download-document.js
@@ -4,7 +4,7 @@
  * to run download-document applet from the project root folder type in your console:
  * > node bin/download-document <client_id> <client_secret> <username> <password> <document_id> <path_to_save>
  * <client_id>, <client_secret>, <username>, <password>, <document_id>, <path_to_save> - are required params
- * options flags:
+ * options:
  * --zip - document will be archived in zip
  * --with-history - document will be downloaded with its history
  */
