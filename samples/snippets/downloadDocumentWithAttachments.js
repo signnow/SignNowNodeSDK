4c770d7e571ddcfc994fe6630656c080827dbbd0
Артем <42999342+Temu4@users.noreply.github.com>
2019-10-08 09:53:17 +0300

    Update samples/snippets/downloadDocumentWithAttachments.js
    Co-Authored-By: Ruslan Tereshchenko <tereshchenko.ruslan@pdffiller.team>

--- samples/snippets/downloadDocumentWithAttachments.js
+++ samples/snippets/downloadDocumentWithAttachments.js
@@ -8,7 +8,7 @@ const { download: downloadDocument } = signnow.document;
 
 const id = 'DOCUMENT_ID_GOES_HERE';
 const token = 'YOUR_ACCESS_TOKEN';
-const options = { zip: true }; // option points on downloading document archived in zip
+const options = { withAttachments: true };
 
 /**
  * @param {Buffer} res - binary data (file) of the entire document
