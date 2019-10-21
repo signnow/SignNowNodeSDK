6aa4b7537c94aaaf9ba4649b85358e65d1603153
Oleksandr Pohrebniak <oleksandr.pohrebniak42@gmail.com>
2019-10-21 16:49:16 +0300

    Update bin/download-document-group.js
    Co-Authored-By: Ruslan Tereshchenko <tereshchenko.ruslan@pdffiller.team>
    
    Update bin/download-document-group.js
    
    Co-Authored-By: Ruslan Tereshchenko <tereshchenko.ruslan@pdffiller.team>
    
    Update bin/download-document-group.js
    
    Co-Authored-By: Ruslan Tereshchenko <tereshchenko.ruslan@pdffiller.team>
    
    Update bin/download-document-group.js
    
    Co-Authored-By: Ruslan Tereshchenko <tereshchenko.ruslan@pdffiller.team>
    
    Update bin/download-document-group.js
    
    Co-Authored-By: Ruslan Tereshchenko <tereshchenko.ruslan@pdffiller.team>
    
    Update bin/download-document-group.js
    
    Co-Authored-By: Ruslan Tereshchenko <tereshchenko.ruslan@pdffiller.team>
    
    Update bin/download-document-group.js
    
    Co-Authored-By: Ruslan Tereshchenko <tereshchenko.ruslan@pdffiller.team>

--- bin/download-document-group.js
+++ bin/download-document-group.js
@@ -2,18 +2,18 @@
 
 /**
  * to run download-document-group applet from the project root folder type in your console:
- * > node bin/download-document-group <client_id> <client_secret> <username> <password> <document_group_id> <path_to_save> <document_order>
+ * > node bin/download-document-group <client_id> <client_secret> <username> <password> <document_group_id> <path_to_save> <...document_order>
  * <client_id>, <client_secret>, <username>, <password>, <document_group_id>, <path_to_save> - are required params
- * <document_order> - is optional param
+ * <...document_order> - is optional param. It stands for one or more document IDs
  * options:
- * --type - type of download flow, can be `zip` or `merged`
- * --with_history - type of history merging flow, can be `no`, `after_each_document`, or `after_merged_pdf`
+ * --type - type of download flow, can be `zip` or `merged`. Defaults to `zip` 
+ * --with-history - type of history merging flow, can be `no`, `after-each-document`, or `after-merged-pdf`. Defaults to `no` 
  */
 
 'use strict';
 
 const args = process.argv.slice(2);
-const flags = args.filter(arg => /^--/.test(arg));
+const options = args.filter(arg => /^--/.test(arg));
 const params = args.filter(arg => !/^--/.test(arg));
 
 const [
@@ -26,12 +26,12 @@ const [
   ...document_order
 ] = params;
 
-const { type, with_history } = flags.reduce((acc, cur) => {
+const { type, with_history } = options.reduce((acc, cur) => {
   const [
     key,
     value,
   ] = cur.split('=');
-  acc[key.slice(2)] = value;
+  acc[key.slice(2).replace(/-/g, '_')] = value;
   return acc;
 }, {});
 
